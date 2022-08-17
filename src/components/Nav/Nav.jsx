d7565f0b000894248c24418c19eb172a4084886d
cfolsom86 <folsomcliff@gmail.com>
2022-08-17 09:34:02 -0500

    changed nav, added portfolio and removed donation

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,6 +5,7 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {GiBookshelf} from 'react-icons/gi'
 import {MdOutlineContactless} from 'react-icons/md'
 import {BiDonateHeart} from 'react-icons/bi'
+import {BsFillHddNetworkFill} from 'react-icons/bs'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -14,8 +15,9 @@ const Nav = () => {
       <a href='#Header' onClick={() => setActiveNav('#Header')} className={activeNav === '#Header' ? 'active' : ''}><FaHouseUser /></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
       <a href='#Experience'onClick={() => setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}><GiBookshelf /></a>
+      <a href='#Portfolio' onClick={() => setActiveNav('#Portfolio')} className={activeNav === '#Portfolio' ? 'active' : ''}><BsFillHddNetworkFill /></a>
       <a href='#Contact' onClick={() => setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><MdOutlineContactless /></a>
-      <a href='#Footer' onClick={() => setActiveNav('#Footer')} className={activeNav === '#Footer' ? 'active' : ''}><BiDonateHeart /></a>
+      {/*<a href='#Footer' onClick={() => setActiveNav('#Footer')} className={activeNav === '#Footer' ? 'active' : ''}><BiDonateHeart /></a>*/}
 
 
     </nav>
