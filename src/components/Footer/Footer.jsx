3975ae3c27884fad2320f3d1e1da7f9d95de5662
cfolsom86 <folsomcliff@gmail.com>
2022-08-23 16:58:41 -0500

    fixed responsive css on nav and header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ import me from '../../assets/folsom-prism_1.png'
 const Footer = () => {
   return (
     <footer>
-      <h1><a href="www.google.com" className='footer__logo'><img src={me} alt="folsom-prism logo"/></a></h1>
+      <h1><a href="https://www.folsom-prism.com" className='footer__logo'><img src={me} alt="folsom-prism logo"/></a></h1>
 
       <ul className='perma__links'>
         <li><a href='#Header'>Header</a></li>
