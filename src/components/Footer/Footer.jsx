af3f6925061741931282cbe36c2ff4d0c3054f72
cfolsom86 <folsomcliff@gmail.com>
2022-08-23 09:17:53 -0500

    added footer and footer.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import './Footer.css'
+import { BsFacebook } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+
+
 
 const Footer = () => {
   return (
-    <div id='Footer'>
-      <h5>&copy; Copyright: 2022 cfolsom86</h5>
-    {/*}  <img src={logo} alt="folsom-prism logo" className='logo'/> */}
-    </div>
+    <footer>
+      <h1><a href="www.google.com" className='footer__logo'>Folsom-Prism</a></h1>
+
+      <ul className='perma__links'>
+        <li><a href='#Header'>Header</a></li>
+        <li><a href='#About'>About</a></li>
+        <li><a href='#Experience'>Experience</a></li>
+        <li><a href='#Services'>Services</a></li>
+        <li><a href='#Portfolio'>Portfolio</a></li>
+        <li><a href='#Testimonials'>Testimonials</a></li>
+        <li><a href='#Contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.https://www.facebook.com/cliff.folsom/"><BsFacebook /></a>
+        <a href="https://www.instagram.com/theonlycliff/"><BsInstagram /></a>
+        <a href="https://www.linkedin.com/in/cliff-folsom-5b331266/"><BsLinkedin /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Clifton Folsom 2022 - All Rights Reserved</small>
+      </div>
+    </footer>
   )
 }
 
