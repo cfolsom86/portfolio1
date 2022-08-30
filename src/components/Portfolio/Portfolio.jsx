3cb5dba2d906f048d517cca2cc3f7ef7973932b1
cfolsom86 <folsomcliff@gmail.com>
2022-08-30 11:53:56 -0500

    added new recipe app

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -46,8 +46,8 @@ const Portfolio = () => {
           </div>
           <h3>Recipe Card Search</h3>
           <div className='portfolio__item-cta'>
-          <a href="https://github.com/cfolsom86/recipe" target="_blank" rel='noopener noreferrer' className='btn'>Github</a>
-          <a href="https://recipes.netlify.app/" target="_blank" rel='noopener noreferrer' className='btn btn-primary'>Live Demo</a>
+          <a href="https://github.com/cfolsom86/cliffs-eats" target="_blank" rel='noopener noreferrer' className='btn'>Github</a>
+          <a href="https://cliffs-eats.netlify.app/" target="_blank" rel='noopener noreferrer' className='btn btn-primary'>Live Demo</a>
           </div>
         </article>  
 
